f3fc12bd233232b1ed6e1e7651105a5ca890b73d
frontinorz <frontinorz@gmail.com>
2021-02-08 11:36:48 +0800

    feat(header): add props  type

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 const Header = (props) => {
   return (
     <header>
@@ -8,6 +10,10 @@ const Header = (props) => {
 
 Header.defaultProps = {
   title: 'Child Header'
+}
+
+Header.propTypes = {
+  title: PropTypes.string,
 }
 
 export default Header
