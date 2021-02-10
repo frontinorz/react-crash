0032b033f05e75694c7beb8024f397ef0d5b83d8
frontinorz <frontinorz@gmail.com>
2021-02-10 17:49:35 +0800

    Add tasks component visibility check

--- src/App.js
+++ src/App.js
@@ -28,10 +28,15 @@ function App() {
   return (
     <div className="container">
       <Header title="Task tracker" />
-      <Tasks
-        tasks={tasks}
-        onDelete={deleteTask}
-      />
+      {
+        tasks.length > 0 ? (
+          <Tasks
+            tasks={tasks}
+            onDelete={deleteTask}
+          />)
+          : (
+            'No tasks to show')
+      }
     </div>
   );
 }
