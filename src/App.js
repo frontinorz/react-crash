51534f1f7f30926c6904c68a51ab5ee286d6a8b5
frontinorz <frontinorz@gmail.com>
2021-02-23 12:59:34 +0800

    Fectch data from json server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
@@ -9,16 +9,33 @@ function App() {
   const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([])
 
+  useEffect(() => {
+    const getTasks = async () => {
+      const taskFromServer = await fetchTasks()
+      setTasks(taskFromServer)
+    }
+    getTasks()
+  }, [])
+
+  // Fetch task from server
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
+  }
+  // Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter(task => task.id != id))
   }
 
+  // Add task
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 1000 + 1)
     const newTask = { id, ...task }
     setTasks([...tasks, newTask])
   }
 
+  // Change task reminder status
   const toggleReminder = (id) => {
     console.log(id)
     setTasks(tasks.map(task =>
