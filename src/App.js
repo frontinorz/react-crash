1c26e931f5258831a3647d23a1f223b088b0c3ec
frontinorz <frontinorz@gmail.com>
2021-02-23 11:48:43 +0800

    Init json-server

--- src/App.js
+++ src/App.js
@@ -7,20 +7,7 @@ import AddTask from './components/AddTask'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: true
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true
-    }
-  ])
+  const [tasks, setTasks] = useState([])
 
   const deleteTask = (id) => {
     setTasks(tasks.filter(task => task.id != id))
